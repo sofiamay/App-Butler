8a2817dff0aa9fff618e5a516ac93c5e8b9c6e94
Sofia Berlin <sofiamberlin@gmail.com>
2016-05-01 20:03:58 -0700

    Update test to use ES6

--- client/test/sample-test.js
+++ client/test/sample-test.js
@@ -3,18 +3,10 @@ import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import App from '../app.js';
 
-describe('app', function () {
-  it('renders without problems', function () {
+describe('app', () => {
+  it('renders without problems', () => {
     const app = TestUtils.renderIntoDocument(<App />);
     expect(app).to.exist;
   });
 });
 
-// var expect = require('chai').expect;
-
-// describe('app', function () {
-//   it('renders without problems', function () {
-//     var app = "something";
-//     expect(app).to.equal("something");
-//   });
-// });
