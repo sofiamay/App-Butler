fb97e273f55a325dcde18b1101a9d0067b568b8d
Sofia Berlin <sofiamberlin@gmail.com>
2016-05-03 15:11:34 -0700

    Set up tests for buildServer

---
+++ test/server/controllers/expressBuild/buildFileTest.js
@@ -0,0 +1,15 @@
+describe('File Builder', () => {
+  it('should call buildMainFile when given a main server file');
+
+  describe('Creation of the main server file', () => {
+    const request = '';
+
+    it('contains code that instantiates express');
+
+    it('contains code that tells the server to listen');
+
+    it('defaults to port 3000 when no port is provided');
+
+    it('adds middleware code to the file if provided');
+  });
+});
--- test/server/userControllerTest.js
+++ test/server/userControllerTest.js
@@ -11,68 +11,63 @@ let { it, before, after, beforeEach, afterEach } = require('arrow-mocha/es5');
 import mongoose from 'mongoose';
 import httpMocks from 'node-mocks-http';
 
-
 let dbURI = 'mongodb://localhost/butler';
 
 // The `clearDB` helper function, when invoked, will clear the database
-let clearDB = (done) => {
-  mongoose.connection.collection.users.remove(done);
+const clearDB = (done) => {
+  mongoose.connection.collections.users.remove(done);
 };
 
 describe('User Controller', () => {
   // Connect to database before any tests
-  before((done) => {
-    if (mongoose.connection.db) {
-      done();
-    }
-    mongoose.connect(dbURI, done);
+
+
+  // Clear database before each test and then seed it with example `users` so that you can run tests
+  beforeEach((done) => {
+    clearDB(() => {
+      const users = [
+        {
+          name: 'Jeremy',
+          email: 'jeremy@jeremy.com',
+          githubID: '1456CD1V76F',
+          key: 'a8s79y987a869df4',
+          files: [],
+        },
+        {
+          name: 'Sofia',
+          email: 'sofia@sofia.com',
+          githubID: '67ACD5429B1',
+          key: '7364929d97639416jlk3847',
+          files: [],
+        },
+        {
+          name: 'Reid',
+          email: 'reid@reid.com',
+          githubID: '72DA845F7E',
+          key: '66785f583867j8934a',
+          files: [],
+        },
+        {
+          name: 'Dylan',
+          email: 'dylan@dylan.com',
+          githubID: 'E498AB3652',
+          key: '65kh2348yqbi2934j03',
+          files: [],
+        },
+        {
+          name: 'Zach',
+          email: 'zach@zach.com',
+          githubID: '26AB346C7C',
+          key: '3476q6234cad86',
+          files: [],
+        },
+      ];
+
+
+      // See http://mongoosejs.com/docs/models.html for details on the `create` method
+      User.create(users, done);
+    });
   });
-
-  // // Clear database before each test and then seed it with example `users` so that you can run tests
-  // beforeEach((done) => {
-  //   clearDB(() => {
-  //     const users = [
-  //       {
-  //         name: 'Jeremy',
-  //         email: 'jeremy@jeremy.com',
-  //         githubID: '1456CD1V76F',
-  //         key: 'a8s79y987a869df4',
-  //         files: [],
-  //       },
-  //       {
-  //         name: 'Sofia',
-  //         email: 'sofia@sofia.com',
-  //         githubID: '67ACD5429B1',
-  //         key: '7364929d97639416jlk3847',
-  //         files: [],
-  //       },
-  //       {
-  //         name: 'Reid',
-  //         email: 'reid@reid.com',
-  //         githubID: '72DA845F7E',
-  //         key: '66785f583867j8934a',
-  //         files: [],
-  //       },
-  //       {
-  //         name: 'Dylan',
-  //         email: 'dylan@dylan.com',
-  //         githubID: 'E498AB3652',
-  //         key: '65kh2348yqbi2934j03',
-  //         files: [],
-  //       },
-  //       {
-  //         name: 'Zach',
-  //         email: 'zach@zach.com',
-  //         githubID: '26AB346C7C',
-  //         key: '3476q6234cad86',
-  //         files: [],
-  //       },
-  //     ];
-
-  //     // See http://mongoosejs.com/docs/models.html for details on the `create` method
-  //     User.create(users, done);
-  //   });
-  // });
 
   it('should have a method that creates a user from the information in the request body', (done) => {
     const request = httpMocks.createRequest({ method: 'POST', url: '/user',
