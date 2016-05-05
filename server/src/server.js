d15a8fe44bb16b20725e6d8addd02503c90218b0
Reid <jreid.greer@gmail.com>
2016-05-05 16:32:10 -0700

    Update to work with merged content

--- server/src/server.js
+++ server/src/server.js
@@ -2,14 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import PrettyError from 'pretty-error';
 import { port } from './config.js';
-<<<<<<< 61680302140cfa5634cd0d38a3a8c8cdb22ee93c
 import passport from 'passport';
 import routes from './router/routes';
 import mongoose from 'mongoose';
 import session from 'express-session';
-=======
 // import browserSync from 'browser-sync';
->>>>>>> Implement Rendering on separate router pages
 
 const server = express();
 const router = routes;
@@ -22,28 +19,15 @@ server.use(session({
   saveUninitialized: true,
 }));
 
-<<<<<<< 61680302140cfa5634cd0d38a3a8c8cdb22ee93c
-server.use(express.static(__dirname + '/../../client'));
-=======
 const pe = new PrettyError();
 pe.start();
 
 server.use(express.static(`${__dirname}/../../client`));
-server.use(bodyParser.urlencoded({ extended: true }));
->>>>>>> Implement Rendering on separate router pages
 server.use(bodyParser.json());
-// server.use(bodyParser.urlencoded({ extended: true }));
 
-<<<<<<< 61680302140cfa5634cd0d38a3a8c8cdb22ee93c
 server.use(passport.initialize());
 router(server, express);
 
-server.listen(port, () => {
-  console.log(`The server is running at http://localhost:${port}`);
-});
-
-export default server;
-=======
 server.listen(port, () => {
   console.log(`The server is running at http://localhost:${port}`);
   // Listen for the `init` event
@@ -58,4 +42,4 @@ server.listen(port, () => {
   // });
 });
 
->>>>>>> Implement Rendering on separate router pages
+export default server;
