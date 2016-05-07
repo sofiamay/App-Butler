0dea9199b2fbb8d916bb7be29b2c0e514fe39ffc
Sofia Berlin <sofiamberlin@gmail.com>
2016-05-07 12:28:37 -0700

    Clean up server

--- server/src/controllers/expressBuild/buildFile.js
+++
@@ -1,7 +0,0 @@
-import { buildMainFile } from './buildMainFile.js';
-
-export function buildFile(fileConfig, userConfig) {
-  // build main server file
-  if (fileConfig.type === 'main') { return buildMainFile(fileConfig, userConfig); }
-  return new Error('Undefined file type');
-}
