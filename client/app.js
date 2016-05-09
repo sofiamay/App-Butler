b5322e0672a80b59577fb2d1479a89716033e275
Dylan Kuang <kuangd.usfcaubms@gmail.com>
2016-05-09 11:59:41 -0700

    Refactor authorization check to check for user_session cookie

--- client/app.js
+++ client/app.js
@@ -11,10 +11,27 @@ import NoMatch from './components/NoMatch.js';
 
 import Store from './store.js';
 
+const getCookie = cookieName => {
+  const name = `${cookieName}=`;
+  const cookies = document.cookie.split(';');
+  for (let i = 0; i < cookies.length; i++) {
+    let cookie = cookies[i];
+    while (cookie.charAt(0) === ' ') {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(name) === 0) {
+      return cookie.substring(name.length, cookie.length);
+    }
+  }
+  return '';
+};
+
 const AuthCheck = (nextState, redirect) => {
-  if (!window.localStorage.user) {
+  // if (!window.localStorage.user) {
+  if (!getCookie('user_session')) {
     redirect({
       pathname: '/*',
+      // Might have to handle state
     });
   }
 };
