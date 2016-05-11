fda373126f60a0c2e08c64e9b20fd328244f617c
Dylan Kuang <kuangd.usfcaubms@gmail.com>
2016-05-11 15:02:06 -0700

    Add tests for github authentication redirect

--- test/server/authentication.js
+++ test/server/authentication.js
@@ -1,27 +1,33 @@
-import chai, { should, expect } from 'chai';
+import chai, { expect } from 'chai';
 import { it } from 'arrow-mocha/es5';
-import httpMocks from 'node-mocks-http';
+
 import supertest from 'supertest';
 
 import express from 'express';
-import bodyParser from 'body-parser';
-import routes from './../../server/src/router/routes';
+
+chai.should();
 
 const server = express();
-const runningServer = server.listen(1337, () => {
-  console.log('Server running at 1337');
+const runningServer = server.listen(8000, () => {
+  console.log('Server running at 8000');
 });
 
 describe('GET /auth/github', () => {
   it('should redirect to github authentication', done => {
+    // expect(true).to.equal(true);
     supertest(server)
     .get('/auth/github')
+    .set('Accept', 'text/html')
     .expect('Content-Type', /html/)
     .end((err, res) => {
-      console.log(res);
-      console.log(err);
+      expect(res.res.statusCode).to.be(200);
       done();
     });
+  });
+  it('should return the content of the html', done => {
+    supertest(server)
+    .get('/')
+    .expect(200, /<input/, done);
   });
 });
 
