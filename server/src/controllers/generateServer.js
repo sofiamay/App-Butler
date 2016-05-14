459a237065478d4221eba16c666bab6fab8d41f9
Dylan Kuang <kuangd.usfcaubms@gmail.com>
2016-05-13 23:02:19 -0700

    Refactor promise chain to create repo and server file

--- server/src/controllers/generateServer.js
+++ server/src/controllers/generateServer.js
@@ -1,6 +1,7 @@
 import { buildAllFiles } from './expressBuild/buildFiles.js';
 import Config from './../models/config.js';
 import { createRepo, createFile } from './githubController.js';
+import { extend } from './../utils/utils';
 
 export function generate(request, response) {
   request.session.files = {
@@ -19,37 +20,50 @@ export function generate(request, response) {
 
   // copy cookies to body.data
   request.body.data.cookies = {};
-  request.body.data.cookies.email = request.cookies.email; // email bug: can't store '@'
-  request.body.data.cookies.userName = request.cookies.user;
-  request.body.data.cookies.token = request.cookies.user_session;
+  extend(request.body.data.cookies, request.cookies);
+  // EMAIL BUG: fix '@'
   const builtFiles = buildAllFiles(request, response);
   // Send these files to github!
   // Make repo (naming handled in controller)
   createRepo(request.body.data).then(() => {
     // separate calls for every file
     // builtFiles will always only be [serverFile, [routerFiles]]
-    builtFiles.forEach(file => {
-      if (!Array.isArray(file)) {
-        createFile(file, request.body.data, request.body.data.cookies, 'server').then(() => {
-          // successfully created server file
-          response.send('Server file successfully created');
-        }).catch(error => {
-          response.status(400).send(`Problem creating server file: Error: ${error}`);
-        });
-      } else {
-        file.forEach((routerFile, ind) => {
-          createFile(routerFile, request.body.data.routers[ind], request.body.data.cookies).then(() => {
-            // successfully created router files
-            response.send('Router files successfully created');
-          }).catch(error => {
-            // error creating router files
-            response.status(400).send(`Problem creating router files: Error: ${error}`);
-          });
-        });
-      }
+    createFile(builtFiles[0], request.body.data, request.body.data.cookies, 'server').then(() => {
+      // successfully created server file
+      builtFiles[1].forEach((file, ind) => {
+        createFile(file, request.body.data.routers[ind], request.body.data.cookies);
+        // .then(() => {
+        //   response.write(`${file} created`);
+        // }).catch(routerError => {
+        //   response.writeHead(400);
+        //   response.write(`Error creating router: ${routerError}`);
+        // });
+      });
+    }).catch(serverError => {
+      response.status(400).send(`Error creating server: ${serverError}`);
     });
+    // builtFiles.forEach(file => {
+    //   if (!Array.isArray(file)) {
+    //     createFile(file, request.body.data, request.body.data.cookies, 'server').then(() => {
+    //       // successfully created server file
+    //       response.send('Server file successfully created');
+    //     }).catch(error => {
+    //       response.status(400).send(`Problem creating server file: Error: ${error}`);
+    //     });
+    //   } else {
+    //     file.forEach((routerFile, ind) => {
+    //       createFile(routerFile, request.body.data.routers[ind], request.body.data.cookies).then(() => {
+    //         // successfully created router files
+    //         response.send('Router files successfully created');
+    //       }).catch(error => {
+    //         // error creating router files
+    //         response.status(400).send(`Problem creating router files: Error: ${error}`);
+    //       });
+    //     });
+    //   }
+    // });
     // successfully created repo
-    response.send(`${request.body.data.appName} created`);
+    // response.send(`${request.body.data.appName} created`);
   }).catch((error) => {
     response.status(400).send(`Problem creating repo on your GitHub: Error: ${error}`);
   });
