d8a457a72a9308fd26295e2c8f29f4b4136a0323
JeremyIR <jeremy.i.rosenfeld@gmail.com>
2016-05-17 22:11:14 -0700

    modify controllers for request body parameters

--- server/src/controllers/configController.js
+++ server/src/controllers/configController.js
@@ -1,11 +1,25 @@
 import Config from '../models/config.js';
+import User from '../models/user.js';
 
 export function createOne(request, response) {
-  const newConfig = new Config({});
-  newConfig.save((err) => {
-    if (err) {
-      return response.status(500).json(err);
-    }
-    return response.json(newConfig);
+  User.findOne({ githubID: 'JeremyIR' }, (err, user) => {
+    const newConfig = new Config({
+      user: user,
+      'data.serverType': request.body.data.serverType,
+      'data.appName': request.body.data.appName,
+      'data.serverSettings.port': request.body.data.serverSettings.port,
+      'data.routers': request.body.data.routers,
+      'data.github.repoName': request.body.data.github.repoName,
+      'data.github.privacy': request.body.data.github.privacy,
+      'data.github.description': request.body.data.github.description,
+    });
+    newConfig.save((err, savedConfig) => {
+      if (err) {
+        return response.status(500).json(err);
+      }
+      return response.send(savedConfig);
+    });
   });
 }
+
+
--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,13 +1,11 @@
 import User from '../models/user.js';
 
-module.exports = {
-  getUser: (req, res) => {
-    User.findOne({ githubID: req.cookies.user }, (err, person) => {
-      if (!person) {
-        res.json(false);
-      } else {
-        res.json(person.id === Number(req.cookies.id));
-      }
-    });
-  },
-};
+export function getUser(req, res) {
+  User.findOne({ githubID: req.cookies.user }, (err, person) => {
+    if (!person) {
+      res.json(false);
+    } else {
+      res.json(person.id === Number(req.cookies.id));
+    }
+  });
+}
