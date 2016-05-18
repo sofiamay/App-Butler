ff2d66719679fef88959eb1a899074abe2316beb
Dylan Kuang <kuangd.usfcaubms@gmail.com>
2016-05-18 16:42:22 -0700

    Dynamically populate saved configuration

--- client/components/Saved.js
+++ client/components/Saved.js
@@ -2,16 +2,24 @@ import React from 'react';
 import Nav from './partials/Nav.js';
 
 export default class Saved extends React.Component {
+  static propTypes = {
+    configurations: React.PropTypes.array,
+  }
+
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      configurations: [],
+    };
 
     fetch('/api/config', {
       method: 'GET',
       credentials: 'same-origin',
     }).then(res => res.json()).then(config => {
-      this.configs = config;
-      console.log(this.configs);
+      console.log(config);
+      this.setState({
+        configurations: config,
+      });
     });
   }
 
@@ -19,7 +27,13 @@ export default class Saved extends React.Component {
     return (
       <div className="container">
         <Nav />
-        sexy page
+        <h1>...your saved configurations</h1>
+        <br />
+        {this.state.configurations.map(config =>
+          <div className="config-list">
+            <a href="/#/design" className="appName">{config.data.appName}</a>
+          </div>
+        )}
       </div>
     );
   }
