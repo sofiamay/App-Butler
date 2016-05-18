667704453c4720ed5b414d58229808026dcf603b
Sofia Berlin <sofiamberlin@gmail.com>
2016-05-18 15:13:27 -0700

    Update config if user already has one with the same name

--- server/src/controllers/configController.js
+++ server/src/controllers/configController.js
@@ -1,24 +1,26 @@
 import Config from '../models/config.js';
-import User from '../models/user.js';
 
+/* Update controller with the given user and appName
+If it doesn't exist, create one */
 export function createOne(request, response) {
-  // User.findOne({ githubID: 'JeremyIR' }, (err, user) => {
-    const newConfig = new Config({
-      user: request.body.user,
-      'data.serverType': request.body.data.serverType,
-      'data.appName': request.body.data.appName,
-      'data.serverSettings.port': request.body.data.serverSettings.port,
-      'data.routers': request.body.data.routers,
-      'data.github.repoName': request.body.data.github.repoName,
-      'data.github.privacy': request.body.data.github.privacy,
-      'data.github.description': request.body.data.github.description,
-    });
-  newConfig.save((err, savedConfig) => {
-    if (err) {
-      return response.status(500).json(err);
-    }
-    return response.send(savedConfig);
-  });
-  // });
-}
+  const oldConfig = { user: request.body.user, appName: request.body.data.appName };
+  const newAttributes = {
+    'data.serverType': request.body.data.serverType,
+    'data.appName': request.body.data.appName,
+    'data.serverSettings.port': request.body.data.serverSettings.port,
+    'data.routers': request.body.data.routers,
+    'data.github.repoName': request.body.data.github.repoName,
+    'data.github.privacy': request.body.data.github.privacy,
+    'data.github.description': request.body.data.github.description,
+  };
 
+  Config.update(
+    oldConfig,
+    newAttributes,
+    { upsert: true },
+    (err, result) => {
+      if (err) { return response.status(500).json(err); }
+      return response.send(result);
+    }
+  );
+}
