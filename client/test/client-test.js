220e8981090b304189c5ed7dbb6c72960acff5c2
Reid <jreid.greer@gmail.com>
2016-05-19 16:59:28 -0700

    Rename client testing for proper identification

--- client/test/client-test.js
+++ client/test/client-test.js
@@ -3,6 +3,13 @@ import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import Index from '../components/Index.js';
 
+describe('Index', () => {
+  it('renders without problems', () => {
+    const app = TestUtils.renderIntoDocument(<Index />);
+    expect(app).to.exist;
+  });
+});
+
 describe('app', () => {
   it('renders without problems', () => {
     const app = TestUtils.renderIntoDocument(<Index />);
