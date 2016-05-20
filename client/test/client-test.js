763e2a69ad9f94f546e37bc45ab46567fdc0433f
Reid <jreid.greer@gmail.com>
2016-05-19 20:38:30 -0700

    Remove unfinished test

--- client/test/client-test.js
+++ client/test/client-test.js
@@ -11,7 +11,7 @@ import Index from '../components/Index.js';
 import Design from '../components/Design.js';
 
 describe('Index', () => {
-  it('renders without problems', () => {
+  it('Renders without problems', () => {
     const app = TestUtils.renderIntoDocument(<Index />);
     expect(app).to.exist;
   });
@@ -19,21 +19,44 @@ describe('Index', () => {
 
 describe('Design Page', () => {
   const design = TestUtils.renderIntoDocument(<Provider store={Store}><Design /></Provider>);
-  it('renders without problems', () => {
+  it('Renders without problems', () => {
     expect(design).to.exist;
   });
-  it('Creates a Router when button is clicked', () => {
-    // First, we find the button container
-    var routerButton = TestUtils.findRenderedDOMComponentWithClass(
-      design,
-      'btn btn-primary'
-    );
-    TestUtils.Simulate.click(routerButton);
-    var router = TestUtils.findRenderedDOMComponentWithClass(
-      design,
-      'routerContainer'
-    );
-    expect(router).to.exist;
+
+  describe('Router Functionality', () => {
+    it('Creates a Router when button is clicked', () => {
+      // First, we find the button
+      var routerButton = TestUtils.findRenderedDOMComponentWithClass(
+        design,
+        'btn btn-primary'
+      );
+      // Simulate a click on the button
+      TestUtils.Simulate.click(routerButton);
+      // Find the generated router
+      var router = TestUtils.findRenderedDOMComponentWithClass(
+        design,
+        'routerContainer'
+      );
+
+      expect(router).to.exist;
+    });
+
+    it('Creates a Endpoint when button on Router is clicked', () => {
+      // First, we find the button
+      var endpointButton = TestUtils.findRenderedDOMComponentWithClass(
+        design,
+        'btn btn-default'
+      );
+      // Simulate a click on the button
+      TestUtils.Simulate.click(endpointButton);
+      // Find the generated router
+      var endpoint = TestUtils.findRenderedDOMComponentWithClass(
+        design,
+        'block block-endpoint'
+      );
+
+      expect(endpoint).to.exist;
+    });
   });
 });
 
