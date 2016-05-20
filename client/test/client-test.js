244570992abd0838b0cea293838ee53b0a838e6a
Reid <jreid.greer@gmail.com>
2016-05-19 17:08:28 -0700

    Implement testing for basic Design rendering

--- client/test/client-test.js
+++ client/test/client-test.js
@@ -1,7 +1,14 @@
 import { expect } from 'chai';
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
+
+// Required Redux Components
+import { Provider } from 'react-redux';
+import Store from '../store.js';
+
+// Tested Components
 import Index from '../components/Index.js';
+import Design from '../components/Design.js';
 
 describe('Index', () => {
   it('renders without problems', () => {
@@ -10,10 +17,10 @@ describe('Index', () => {
   });
 });
 
-describe('app', () => {
+describe('Design Page', () => {
   it('renders without problems', () => {
-    const app = TestUtils.renderIntoDocument(<Index />);
-    expect(app).to.exist;
+    const design = TestUtils.renderIntoDocument(<Provider store={Store}><Design /></Provider>);
+    expect(design).to.exist;
   });
 });
 
